e4aab285abc01efeba78e168273a9a34d7ac2765
okki jaya <okkijaya@okkis-macbook.attlocal.net>
2016-03-25 21:29:20 -0700

    second

--- www/js/app.js
+++ www/js/app.js
@@ -1,103 +1,141 @@
-// Ionic Starter App
+/**
+ * Admin for Ionic Shop ++
+ * 
+ * @version: v2.0
+ * @date: 2016-02-26
+ * @author: Noodlio <noodlio@seipel-ibisevic.com>
+ * @website: www.noodl.io
+ * 
+*/
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngStorage', 'ngCordova', 'firebase', 'ngMessages'])
+// !important settings
+// Please fill in the following constants to get the project up and running
+//
+var FBURL           = 'https://testwlimit.firebaseio.com/';  
+var LIMITVALUE      = 10000000;
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+angular.module('noodlio', [
+  'ui.router', 
+  'firebase',
+  'naif.base64',
+  'btford.markdown',
+  'noodlio.controllers-account',
+  'noodlio.controllers-home',
+  'noodlio.controllers-categories',
+  'noodlio.controllers-settings-fees',
+  'noodlio.controllers-items',
+  'noodlio.controllers-sales',
+  'noodlio.controllers-navbar',
+  'noodlio.controllers-submit',
+  'noodlio.services-auth',
+  'noodlio.services-categories',    // todo: put categories in settings
+  'noodlio.services-settings',
+  'noodlio.services-products',
+  'noodlio.services-orders',
+  'noodlio.services-utils',
+  ]
+)
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+  
+    //$locationProvider.html5Mode(true);
+    //$locationProvider.hashPrefix('!');
+    $urlRouterProvider.otherwise('/admin/home');
+    $stateProvider
+
+    // abstract state in the form of a navbar
+    .state('admin', {
+        url: '/admin',
+        templateUrl: '/templates/navbar.html',
+        abstract: true,
+        controller:'NavBarCtrl as navbar',
+    })
+    
+    // home
+    .state('admin.home', {
+        url: '/home',
+        templateUrl: '/templates/home.html',
+        controller:'HomeCtrl as home',
+
+    })
+    
+    .state('admin.categories', {
+        url: '/categories',
+        templateUrl: '/templates/categories.html',
+        controller:'CategoriesCtrl as categories',
+    })
+    
+    .state('admin.settings-fees', {
+        url: '/settings/fees',
+        templateUrl: '/templates/settings-fees.html',
+        controller:'SettingsFeesCtrl as settings',
+    })
+    
+    .state('admin.login', {
+        url: '/login',
+        templateUrl: '/templates/login.html',
+        controller:'AccountCtrl as account',
+    })
+    
+    .state('admin.items', {
+        url: '/items',
+        templateUrl: '/templates/items.html',
+        controller:'ItemsCtrl as items',
+    })
+    
+    .state('admin.sales', {
+        url: '/sales',
+        templateUrl: '/templates/sales.html',
+        controller:'SalesCtrl as sales',
+    })
+    .state('admin.sales-detail', {
+        url: '/sales/:index/:orderId',
+        templateUrl: '/templates/sales-detail.html',
+        controller:'SalesDetailCtrl as sdetail',
+    })
+    
+    .state('admin.submit', {
+        url: '/submit/:productId',
+        templateUrl: '/templates/submit.html',
+        controller:'SubmitCtrl as submit',
+    })
+    
+    
+    /**
+    
+    
+    
+    .state('admin.featured', {
+        url: '/featured',
+        templateUrl: '/templates/featured.html',
+        controller:'ItemsCtrl as items',
+    })
+    
+    .state('admin.personal', {
+        url: '/personal',
+        templateUrl: '/templates/personal.html',
+        controller:'PersonalCtrl as personal',
+    })
+    */
+
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
 
-  // Home screen
-  .state('home', {
-    url: '/home',
-    templateUrl: 'templates/home.html',
-    controller: 'HomeCtrl'
-  })
-
-  // View category
-  .state('category', {
-    url: '/category',
-    templateUrl: 'templates/category.html',
-    controller: 'CategoryCtrl'
-  })
-
-  // Product detail
-  .state('detail', {
-    url: '/detail',
-    templateUrl: 'templates/detail.html',
-    controller: 'DetailCtrl'
-  })
-
-  // Cart detail
-  .state('cart', {
-    url: '/cart',
-    templateUrl: 'templates/cart.html',
-    controller: 'CartCtrl'
-  })
-
-  // Checkout steps
-  // Address
-  .state('checkout_address', {
-    url: '/checkout/address',
-    templateUrl: 'templates/checkout-address.html',
-    controller: 'CheckoutCtrl'
-  })
-
-  // Shipping
-  .state('checkout_shipping', {
-    url: '/checkout/shipping',
-    templateUrl: 'templates/checkout-shipping.html',
-    controller: 'CheckoutCtrl'
-  })
-
-  // Payment
-  .state('checkout_payment', {
-    url: '/checkout/payment',
-    templateUrl: 'templates/checkout-payment.html',
-    controller: 'CheckoutCtrl'
-  })
-
-  // login screen
-  .state('login', {
-    url: '/login',
-    templateUrl: 'templates/login.html',
-    controller: 'loginCtrl'
-  })
-
-  // register screen
-  .state('register', {
-    url: '/register',
-    templateUrl: 'templates/register.html',
-    controller: 'registerCtrl'
-  });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/login');
-
+.directive('itemCols', function() {
+  return {
+    templateUrl: 'templates/directives/item-cols.html'
+  };
 })
-  .constant('FURL', 'https://spiffbeta.firebaseio.com/');
 
+.directive('attributeSettings', function() {
+  return {
+    templateUrl: 'templates/directives/attribute-settings.html'
+  };
+})
+
+.directive('checkoutCartOverview', function() {
+  return {
+    templateUrl: 'templates/directives/checkout-cart-overview.html'
+  };
+})
